b74589d355b75daea119146f8801723ba21d3b45
cinnamon-crew <maestrocinna@gmail.com>
2023-10-13 11:54:47 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,22 @@
+// //ESM
+// //CommonJS
+
+// //import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// //export  {
+// //   plugins:[
+// //     autoprefixer
+// //   ]
+// // }
+
+// module.exports = {
+//   plugins:[
+//     autoprefixer
+//   ]
+// }
+
+//간소화
+module.exports = {
+  plugins: [require("autoprefixer")],
+};
